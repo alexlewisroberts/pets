5f729a4a2d2a19cc20c228d9635474503d8fd3e4
Alex Roberts <alexlewisroberts@gmail.com>
2018-07-24 17:17:16 -0700

    Initial Commit

---
+++ collection/CollectionTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CollectionTableViewController.swift
+//  collection
+//
+//  Created by Alex Roberts on 7/23/18.
+//  Copyright © 2018 Lambda School Inc. All rights reserved.
+//
+
+import UIKit
+
+class CollectionTableViewController: UITableViewController {
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		tableView.reloadData()
+	}
+
+
+	// MARK: - Table view data source
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return list.count
+	}
+	
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "CollectionCell", for: indexPath)
+		
+		// Configure the cell...
+		let item = list[indexPath.row]
+		cell.textLabel?.text = item
+		
+		return cell
+	}
+	
+
+	// MARK: - Properties
+	let list = ["giraffe","elephant","sea urchin"]
+}
